1f9b84c0ca1e5ce265e5ce85d64c8adfba4353ed
louisx2 <loui-s@hotmail.com>
2025-08-03 22:49:15 -0400

    Update Program.cs

--- SellAlleS.API/Program.cs
+++ SellAlleS.API/Program.cs
@@ -1,1 +1,13 @@
-// Entry point configuration
+// Entry point configurationusing SellAlleS.API.Datos;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Agregar EF y conexión
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Agregar Swagger y controladores
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
